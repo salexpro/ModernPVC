4836e27de99b1ebb0ffc072cad52849cfbbb43a8
salexpro <dev@salex.pro>
2016-11-01 00:00:45 +0200

    added old price to services

--- index.php
+++ index.php
@@ -1,95 +1,97 @@
 <?php
-defined('_JEXEC') or die;
-$app = JFactory::getApplication();
-$doc = JFactory::getDocument();
-$menu = $app->getMenu();
-$lang = JFactory::getLanguage();
+    defined('_JEXEC') or die;
+    $app = JFactory::getApplication();
+    $doc = JFactory::getDocument();
+    $menu = $app->getMenu();
+    $lang = JFactory::getLanguage();
 
-$template_url = $this->baseurl . '/templates/' . $this->template;
-$doc->addStyleSheet($template_url . '/css/app.css');
-$page_id = $menu->getActive()->id;
-$is_homepage = $menu->getActive() == $menu->getDefault($lang->getTag());
-$dontInclude = array(
-'/media/jui/js/jquery.js',
-'/media/jui/js/jquery.min.js',
-'/media/jui/js/jquery-noconflict.js',
-'/media/jui/js/jquery-migrate.js',
-'/media/jui/js/jquery-migrate.min.js',
-'/media/jui/js/bootstrap.js',
-'/media/system/js/core-uncompressed.js',
-'/media/system/js/tabs-state.js',
-'/media/system/js/core.js',
-'/media/system/js/mootools-core.js',
-'/media/system/js/mootools-core-uncompressed.js',
-'/media/system/js/caption.js'
-);
+    $template_url = $this->baseurl . '/templates/' . $this->template;
+    $doc->addStyleSheet($template_url . '/css/app.css');
+    $page_id = $menu->getActive()->id;
+    $is_homepage = $menu->getActive() == $menu->getDefault($lang->getTag());
+    $dontInclude = array(
+        '/media/jui/js/jquery.js',
+        '/media/jui/js/jquery.min.js',
+        '/media/jui/js/jquery-noconflict.js',
+        '/media/jui/js/jquery-migrate.js',
+        '/media/jui/js/jquery-migrate.min.js',
+        '/media/jui/js/bootstrap.js',
+        '/media/system/js/core-uncompressed.js',
+        '/media/system/js/tabs-state.js',
+        '/media/system/js/core.js',
+        '/media/system/js/mootools-core.js',
+        '/media/system/js/mootools-core-uncompressed.js',
+        '/media/system/js/caption.js'
+    );
 
-foreach($doc->_scripts as $key => $script){
-    if(in_array($key, $dontInclude)){
-        unset($doc->_scripts[$key]);
+    foreach($doc->_scripts as $key => $script){
+        if(in_array($key, $dontInclude)){
+            unset($doc->_scripts[$key]);
+        }
     }
-}
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html>
-<head>
-    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <meta name="viewport" content="width=device-width">
-    <meta name="theme-color" content="#f2f2f2"/>
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=cyrillic" rel="stylesheet">
-    <jdoc:include type="head" />
-    <?php echo '<script>var yaParams = {ip_adress: "'. $_SERVER['REMOTE_ADDR'] .'" };</script>'; ?>
-</head>
-<body>
-    <main class="wrap">
-        <jdoc:include type="modules" name="header" />
-        <nav class="nav" data-closable>
-            <div class="row align-middle align-justify">
-                <jdoc:include type="modules" name="menu" />
-                <jdoc:include type="modules" name="share" />
-            </div>
-            <button class="show-for-small-only close-button" aria-label="Закрыть окно" type="button" data-close><span aria-hidden="true">&times;</span></button>
-        </nav>
-        <?if($is_homepage){?>
-            <section class="slider">
-                <jdoc:include type="modules" name="slider" />
-                <jdoc:include type="modules" name="call" />
-            </section>
-            <jdoc:include type="modules" name="services" />
-            <jdoc:include type="modules" name="products" />
-            <jdoc:include type="modules" name="callsect" />
-            <jdoc:include type="modules" name="why" />
-            <jdoc:include type="modules" name="about" />
-            <jdoc:include type="modules" name="how" />
-            <jdoc:include type="modules" name="portfolio" />
-            <jdoc:include type="modules" name="reviews" />
-        <?}else{?>
-            <section class="row page">
-                <aside class="small-12 medium-4 large-3 column leftmenu">
-                    <div class="leftmenu_inner">
-                        <jdoc:include type="modules" name="sidebar" style="sidebar" />
-                    </div>
-                </aside>
-                <article class="columns">
-                    <jdoc:include type="message"/>
-                    <jdoc:include type="component"/>
-                </article>         
-            </section>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#f2f2f2"/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=cyrillic" rel="stylesheet">
+        <jdoc:include type="head" />
+        <?php echo '<script>var yaParams = {ip_adress: "'. $_SERVER['REMOTE_ADDR'] .'" };</script>'; ?>
+    </head>
+    <body>
+        <main class="wrap">
+            <jdoc:include type="modules" name="header" />
+            <nav class="nav" data-closable>
+                <div class="row align-middle align-justify">
+                    <jdoc:include type="modules" name="menu" />
+                    <jdoc:include type="modules" name="share" />
+                </div>
+                <button class="show-for-small-only close-button" aria-label="Закрыть окно" type="button" data-close><span aria-hidden="true">&times;</span></button>
+            </nav>
+            <?if($is_homepage){?>
+                <section class="slider">
+                    <jdoc:include type="modules" name="slider" />
+                    <jdoc:include type="modules" name="call" />
+                </section>
+                <jdoc:include type="modules" name="services" />
+                <jdoc:include type="modules" name="products" />
+                <jdoc:include type="modules" name="callsect" />
+                <jdoc:include type="modules" name="why" />
+                <jdoc:include type="modules" name="about" />
+                <jdoc:include type="modules" name="how" />
+                <jdoc:include type="modules" name="portfolio" />
+                <jdoc:include type="modules" name="reviews" />
+            <?}else{?>
+                <section class="row page">
+                    <aside class="small-12 medium-4 large-3 column leftmenu">
+                        <div class="leftmenu_inner">
+                            <jdoc:include type="modules" name="sidebar" style="sidebar" />
+                        </div>
+                    </aside>
+                    <article class="columns">
+                        <jdoc:include type="message"/>
+                        <jdoc:include type="component"/>
+                    </article>         
+                </section>
+            <?}?>
+            <jdoc:include type="modules" name="footer" />
+        </main>
+        <?if($page_id!=359){?>
+        <script src="<?echo $template_url?>/bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="<?echo $template_url?>/bower_components/foundation-sites/dist/foundation.min.js"></script>
+        <script>$(document).foundation()</script>
         <?}?>
-        <jdoc:include type="modules" name="footer" />
-    </main>
-    <?if($page_id!=359){?>
-    <script src="<?echo $template_url?>/bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="<?echo $template_url?>/bower_components/foundation-sites/dist/foundation.min.js"></script>
-    <script>$(document).foundation()</script>
-    <?}?>
-    <?if($is_homepage){?>
-    <script src="<?echo $template_url?>/bower_components/bxslider-4/dist/jquery.bxslider.min.js"></script>
-    <script>var ismain = true</script>   
-    <?} else {?>
-    <script>var ismain = false</script>   
-    <?}?>
-    <script src="<?echo $template_url?>/js/app.js"></script>
-</body>
+        <?if($is_homepage){?>
+        <script src="<?echo $template_url?>/bower_components/bxslider-4/dist/jquery.bxslider.min.js"></script>
+        <script>var ismain = true</script>   
+        <?} else {?>
+        <script>var ismain = false</script>   
+        <?}?>
+        <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+        <script src="//yastatic.net/share2/share.js"></script>
+        <script src="<?echo $template_url?>/js/app.js"></script>
+    </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -41,78 +41,99 @@ $('.reviews_item_text button').click(function(){
 
 var windows = {
     VEKAWHS60: {
+        "price_2_old": "0 000",
         "price_2": "5 700",
+        "price_3_old": "0 000",
         "price_3": "7 300",
+        "price_b_old": "0 000",
         "price_b": "12 163",
         "silent": "60",
-        "warm"  : "75",
+        "warm"  : "60",
         "light" : "60",
         "desc_2": "<b>Размер:</b> 1200x1200мм<br><b>Поворотная створка:</b> 600x1200мм<br><b>Глухая створка:</b> 600x1200мм<br><b>Цена изделия:</b> без монтажа и отделочных<br>работ, и дополнительных элементов.<br><span>ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1200мм<br><b>Повортная створка:</b> 600x1200мм<br><b>Глухая створка:</b> 600x1200мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Глухая и повортная створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных<br>работ, и дополнительных элементов.<br><span>ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый"
     },
     VEKAWHS72: {
+        "price_2_old": "0 000",
         "price_2": "7 100",
+        "price_3_old": "0 000",
         "price_3": "9 100",
+        "price_b_old": "0 000",
         "price_b": "14 629",
-        "silent": "60",
-        "warm"  : "75",
-        "light" : "60",
+        "silent": "80",
+        "warm"  : "80",
+        "light" : "50",
         "desc_2": "<b>Размер:</b> 1200x1200мм<br><b>Поворотная створка:</b> 600x1200мм<br><b>Глухая створка:</b> 700x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных<br>работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1200мм<br><b>Повортно-откидная створка:</b>600x1200мм<br><b>Глухая створка:</b> 600x1200мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Глухая и повортная створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных<br>работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый"
     },
     VEKAEUROLINE: {
+        "price_2_old": "0 000",
         "price_2": "8 786",
+        "price_3_old": "0 000",
         "price_3": "10 740",
+        "price_b_old": "0 000",
         "price_b": "17 198",
         "silent": "60",
-        "warm"  : "75",
-        "light" : "60",
+        "warm"  : "60",
+        "light" : "40",
         "desc_2": "<b>Размер:</b> 1400x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Глухая створка:</b> 700x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1400мм<br><b>Повортно-откидная створка:</b>600x1400мм<br><b>Глухая створка:</b> 600x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Глухая и повортная створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый"
     },
     VEKASOFTLINE: {
+        "price_2_old": "0 000",
         "price_2": "9 598",
+        "price_3_old": "0 000",
         "price_3": "11 735",
+        "price_b_old": "0 000",
         "price_b": "19 385",
-        "silent": "60",
-        "warm"  : "75",
-        "light" : "60",
+        "silent": "80",
+        "warm"  : "80",
+        "light" : "20",
         "desc_2": "<b>Размер:</b> 1400x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Глухая створка:</b> 700x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1400мм<br><b>Повортно-откидная створка:</b>600x1400мм<br><b>Глухая створка:</b> 600x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Глухая и повортная створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный ТЕПЛОПАКЕТ</span><br><b>Цвет:</b> белый"
     },
     KBEGUTWERK: {
+        "price_2_old": "0 000",
         "price_2": "7 830",
+        "price_3_old": "0 000",
         "price_3": "9 358",
+        "price_b_old": "0 000",
         "price_b": "13 283",
         "silent": "60",
-        "warm"  : "75",
+        "warm"  : "40",
         "light" : "60",
         "desc_2": "<b>Размер:</b> 1400x1400мм<br><b>Глухая створка:</b> 700x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1400мм<br><b>Повортно-откидная створка:</b>600x1400мм<br><b>Глухая створка:</b> 600x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Глухая и повортная створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый"
     },
     KBEENERGY: {
+        "price_2_old": "",
         "price_2": "8 676",
+        "price_3_old": "0 000",
         "price_3": "10 310",
+        "price_b_old": "0 000",
         "price_b": "14 937",
-        "silent": "60",
+        "silent": "80",
         "warm"  : "75",
-        "light" : "60",
+        "light" : "40",
         "desc_2": "<b>Размер:</b> 1400x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1400мм<br><b>Повортно-откидная створка:</b>600x1400мм<br><b>Поворотная створка:</b> 600x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Две повортные створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый"
     },
     KBE76: {
+        "price_2_old": "0 000",
         "price_2": "10 028",
+        "price_3_old": "0 000",
         "price_3": "11 928",
+        "price_b_old": "0 000",
         "price_b": "17 556",
-        "silent": "60",
-        "warm"  : "75",
-        "light" : "60",
+        "silent": "100",
+        "warm"  : "100",
+        "light" : "20",
         "desc_2": "<b>Размер:</b> 1400x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Поворотная створка:</b> 700x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый",
         "desc_3": "<b>Размер:</b> 1800x1400мм<br><b>Повортно-откидная створка:</b>600x1400мм<br><b>Поворотная створка:</b> 600x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый",
         "desc_b": "<b>Размер:</b> 1830x2160мм<br><b>Поворотная створка:</b> 670x2160мм<br><b>Две повортные створки:</b> 1160x1400мм<br><b>Цена изделия:</b> без монтажа и отделочных работ, и дополнительных элементов.<br><span>Двухкамерный стеклопакет</span><br><b>Цвет:</b> белый"
@@ -127,27 +148,31 @@ $(".service_list > li").click(function() {
     $(this).addClass("active");
 
     // Smena ceni
-    $('.service_item:first .service_item_price').html('<span>'+windows[wnd].price_2+'</span> руб.');
-    $('.service_item:eq(1) .service_item_price').html('<span>'+windows[wnd].price_3+'</span> руб.');
-    $('.service_item:last .service_item_price').html('<span>'+windows[wnd].price_b+'</span> руб.');
+    $('.service_item:first-child .service_item_price_old').html((windows[wnd].price_2_old) ? windows[wnd].price_2_old+' руб.' : '');
+    $('.service_item:nth-child(2) .service_item_price_old').html((windows[wnd].price_3_old) ? windows[wnd].price_3_old+' руб.' : '');
+    $('.service_item:last-child .service_item_price_old').html((windows[wnd].price_b_old) ? windows[wnd].price_b_old+' руб.' : '');
+
+    $('.service_item:first-child .service_item_price').html('<span>'+windows[wnd].price_2+'</span> руб.');
+    $('.service_item:nth-child(2) .service_item_price').html('<span>'+windows[wnd].price_3+'</span> руб.');
+    $('.service_item:last-child .service_item_price').html('<span>'+windows[wnd].price_b+'</span> руб.');
 
     // Смена параметров
-    $('.service_item:first .service_item_params div:first .progress-meter').css('width',windows[wnd].silent+'%');
-    $('.service_item:first .service_item_params div:eq(1) .progress-meter').css('width',windows[wnd].warm+'%');
-    $('.service_item:first .service_item_params div:last .progress-meter').css('width',windows[wnd].light+'%');
+    $('.service_item:first-child .service_item_params div:first-child .progress-meter').css('width',windows[wnd].silent+'%');
+    $('.service_item:first-child .service_item_params div:nth-child(2) .progress-meter').css('width',windows[wnd].warm+'%');
+    $('.service_item:first-child .service_item_params div:last-child .progress-meter').css('width',windows[wnd].light+'%');
 
-    $('.service_item:eq(1) .service_item_params div:first .progress-meter').css('width',windows[wnd].silent+'%');
-    $('.service_item:eq(1) .service_item_params div:eq(1) .progress-meter').css('width',windows[wnd].warm+'%');
-    $('.service_item:eq(1) .service_item_params div:last .progress-meter').css('width',windows[wnd].light+'%');
+    $('.service_item:nth-child(2) .service_item_params div:first-child .progress-meter').css('width',windows[wnd].silent+'%');
+    $('.service_item:nth-child(2) .service_item_params div:nth-child(2) .progress-meter').css('width',windows[wnd].warm+'%');
+    $('.service_item:nth-child(2) .service_item_params div:last-child .progress-meter').css('width',windows[wnd].light+'%');
 
-    $('.service_item:last .service_item_params div:first .progress-meter').css('width',windows[wnd].silent+'%');
-    $('.service_item:last .service_item_params div:eq(1) .progress-meter').css('width',windows[wnd].warm+'%');
-    $('.service_item:last .service_item_params div:last .progress-meter').css('width',windows[wnd].light+'%');
+    $('.service_item:last-child .service_item_params div:first-child .progress-meter').css('width',windows[wnd].silent+'%');
+    $('.service_item:last-child .service_item_params div:nth-child(2) .progress-meter').css('width',windows[wnd].warm+'%');
+    $('.service_item:last-child .service_item_params div:last-child .progress-meter').css('width',windows[wnd].light+'%');
 
     // Smena soderjimogo
-    $('.service_item:first .service_item_desc').html(windows[wnd].desc_2);
-    $('.service_item:eq(1) .service_item_desc').html(windows[wnd].desc_3);
-    $('.service_item:last .service_item_desc').html(windows[wnd].desc_b);
+    $('.service_item:first-child .service_item_desc').html(windows[wnd].desc_2);
+    $('.service_item:nth-child(2) .service_item_desc').html(windows[wnd].desc_3);
+    $('.service_item:last-child .service_item_desc').html(windows[wnd].desc_b);
 });
 
 // Адаптивность
